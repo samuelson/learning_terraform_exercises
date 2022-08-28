7e817592c9ce914b0549b105ce9781604cf2cbe9
Josh Samuelson <josh@joshsamuelson.com>
2022-08-28 11:34:34 -0700

    Starter Code for Learning Terraform

---
+++ variables.tf
@@ -0,0 +1,9 @@
+#variable "region" {
+#  description = "AWS region"
+#  default     = "us-west-2"
+#}
+
+#variable "instance_type" {
+#  description = "Type of EC2 instance to provision"
+#  default     = "t3.nano"
+#}
