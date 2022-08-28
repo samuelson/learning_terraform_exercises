2e84a24c1e8bd89b680a2869126ada2d81a232e1
Josh Samuelson <josh@joshsamuelson.com>
2022-08-28 11:54:33 -0700

    02_01 Solution

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-#variable "region" {
-#  description = "AWS region"
-#  default     = "us-west-2"
-#}
-
 #variable "instance_type" {
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
